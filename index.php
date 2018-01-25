ea8fe26a20f4804e3aed098522d4836dc88f23b6
nguyenky <lenguyenky1801@gmail.com>
2018-01-25 19:59:31 +0700

    1

--- index.php
+++ index.php
@@ -2,10 +2,33 @@
 <html>
 <head>
 	<title></title>
-	<link rel="stylesheet" href="app.css">
+	
 	<link rel="stylesheet" href="reset.css">
+	<link rel="stylesheet" href="app.css">
 </head>
 <body>
-	<h1 class="title">hello</h1>
+	<div>
+		<div class="header">
+			<div class="header-item">
+				<div class="image">
+					<img src="image/vn.png">
+				</div>
+				<div class="goal">
+					0
+				</div>
+			</div>
+			<div class="header-item">
+				<div class="goal">
+					0
+				</div>
+				<div class="image">
+					<img src="image/uzb.png">
+				</div>
+			</div>
+		</div>
+		<div class="body">
+			<button class="btn">Bắt đầu</button>
+		</div>
+	</div>
 </body>
 </html>
